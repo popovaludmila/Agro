9a09cb349d6c7d190f2a9b30297e995e82a84179
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2020-10-22 17:57:16 +0300

    Измененная версия

---
+++ source/js/main.js
@@ -0,0 +1,13 @@
+const navMain = document.querySelector('.main-nav'),
+    navToggle = document.querySelector('.main-nav__toggle');
+
+    navToggle.addEventListener('click', function () {
+        if (navMain.classList.contains('main-nav--closed')) {
+          navMain.classList.remove('main-nav--closed');
+          navMain.classList.add('main-nav--opened');
+        } else {
+          navMain.classList.add('main-nav--closed');
+          navMain.classList.remove('main-nav--opened');
+          }
+      });
+
